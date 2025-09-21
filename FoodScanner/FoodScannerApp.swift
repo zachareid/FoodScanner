63b871d749bdd32e23bc4fe242d27084f8c9e59e
Zac <zacreid45@gmail.com>
2025-09-20 23:24:03 -0700

    Initial Commit

---
+++ FoodScanner/FoodScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodScannerApp.swift
+//  FoodScanner
+//
+//  Created by Zachary Reid on 9/20/25.
+//
+
+import SwiftUI
+
+@main
+struct FoodScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
